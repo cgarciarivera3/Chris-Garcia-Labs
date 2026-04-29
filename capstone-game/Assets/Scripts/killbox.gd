481b25769bd066ba0db066b0f65e47202d5845b7
cgarciarivera3 <cgarciarivera@fordham.edu>
2026-04-29 10:51:59 -0400

    Finished Level 1 Design

---
+++ capstone-game/Assets/Scripts/killbox.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
